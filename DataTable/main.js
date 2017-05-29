8fe9a16fb9179b93edc2f92f658c458bd94cce8b
Wenjin Liu <Wenjin Liu>
2017-05-29 22:53:32 +0800

    创建组件..

---
+++ DataTable/main.js
@@ -0,0 +1,87 @@
+﻿(function () {
+  var config = {
+    css : "style.css"
+  }
+
+  /*
+  var data = {
+  header: [{
+  "name" : "代祷者",
+  "key" : "prayer"
+  }, {
+  "name" : "事项",
+  "key" : "content"
+  }, {
+  "name" : "日期",
+  "key"  : "date"
+  }, {
+  "name" : "操作",
+  "key"  : "operation",
+  render : function(value, data) {
+  return "<a>删除</a>"
+  }
+  }
+  ],
+  records : [{
+  "prayer" : "刘文金",
+  "content" : "为教会的合一与发展祷告",
+  "date" : "2016-10-12"
+  }, {
+  "prayer" : "刘文金",
+  "content" : "为教会的合一与发展祷告",
+  "date" : "2016-10-12"
+  }, {
+  "prayer" : "刘文金",
+  "content" : "为教会的合一与发展祷告",
+  "date" : "2016-10-12"
+  }
+  ]
+  }
+   */
+
+  webCpu.regComponent("DataTable", config, function (container, data, task) {
+    container.innerHTML = "";
+    var table = document.createElement("table");
+    table.setAttribute("class", "table");
+
+    if (typeof(data.caption) !== "undefined") {
+      var caption = document.createElement("caption");
+      caption.innerHTML = data.caption;
+      table.appendChild(caption);
+    }
+    if (data.header && data.header.length > 0 && data.records) {
+      var thead = document.createElement("thead");
+      var tbody = document.createElement("tbody");
+      table.appendChild(thead);
+      table.appendChild(tbody);
+      var tr = document.createElement("tr");
+      for (var i = 0; i < data.header.length; i++) {
+        var th = document.createElement("th");
+        th.innerHTML = data.header[i].name;
+        tr.appendChild(th);
+      }
+      thead.appendChild(tr);
+	  var count = i;
+      for (var i = 0; i < data.records.length; i++) {
+        var tr = document.createElement("tr");
+        for (var j = 0; j < data.header.length; j++) {
+          var td = document.createElement("td");
+          if (typeof(data.header[j].render) === "function") {
+            td.innerHTML = data.header[j].render(data.records[i][data.header[j].key], data.records[i]);
+          } else {
+            td.innerHTML = data.records[i][data.header[j].key];
+          }
+          tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+      }
+	  if(i===0) {
+		var tr = document.createElement("tr");
+		tr.innerHTML = "<td colspan='"+count+"'><p class='DataTable_emptyTip'>当前列表为空</p></td>";
+		tbody.appendChild(tr);
+	  }
+      container.appendChild(table);
+    }
+  });
+
+})();
---
+++ NavPanel/main.js
@@ -0,0 +1,49 @@
+﻿(function () {
+  var config = {
+    css : "style.css"
+  } 
+  webCpu.regComponent("NavPanel", config, function(container, data, task){
+	var nav = document.createElement("ul");
+	nav.setAttribute("class", "NavIndex");
+	container.appendChild(nav);
+	var sheet = document.createElement("div");
+	sheet.setAttribute("class", "NavSheetArea");
+	container.appendChild(sheet);
+	task.sheets = {};
+	task.navItems = {};
+	for(var i = 0; i < data.length; i++) {
+	  var li = document.createElement("li");
+	  li.innerHTML = data[i];
+	  li.setAttribute("flag", data[i]);
+	  nav.appendChild(li);
+	  var sheetItem = document.createElement("div");
+	  sheetItem.setAttribute("class", "NavSheet");
+	  sheet.appendChild(sheetItem);
+	  var _sheetItem = document.createElement("div");
+	  _sheetItem.setAttribute("class", "_NavSheet");
+	  sheetItem.appendChild(_sheetItem);
+	  task.sheets[data[i]] = _sheetItem;
+	  task.navItems[data[i]] = li;
+	}
+	
+	var navIndex = $(container).find(".NavIndex li");  
+	var navSheet = $(container).find(".NavSheet");
+	navIndex.on("click", function(){
+	  navIndex.removeClass("active");	
+	  $(this).addClass("active");	
+	  var index = $(this).index();
+	  navSheet.hide();
+	  navSheet.eq(index).show();
+	  if(typeof(task.promise.switchPanel) === "function") {
+		task.promise.switchPanel(navSheet.eq(index)[0], $(this).attr("flag"), task);  
+	  }
+	})
+	navIndex.eq(0).click();
+  }); 
+  
+  webCpu["NavPanel"].switchNavPanel = function(container, n) {
+	var navIndex = $(container).find(".NavIndex li");  
+	navIndex.eq(n-1).click();  
+  }
+
+})();
