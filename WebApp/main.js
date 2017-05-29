2e65b13ec4fc821346226d181c9727823d7bdf26
Wenjin Liu <Wenjin Liu>
2017-05-29 23:00:23 +0800

    创建组件

---
+++ WebApp/main.js
@@ -0,0 +1,34 @@
+﻿(function () {
+  var config = {
+    // css: "style.css"
+  }
+
+  webCpu.regComponent("WebApp", config, function (container, data, task) {
+	
+	var iframes = container.getElementsByTagName("iframe"); 
+    if (iframes.length === 0) {
+      task.iframe = document.createElement("iframe");
+      task.iframe.setAttribute("frameborder", 0);
+      task.iframe.setAttribute("width", "100%");
+      task.iframe.setAttribute("height", "100%");
+      container.appendChild(task.iframe);
+    }
+	else {
+	  task.iframe = iframes[0];	
+	}
+    if (task.sandbox) {
+      task.iframe.src = task.sandbox;
+      task.iframe.onload = function () {
+        var script = document.createElement("script");
+        script.src = task.data.url;
+        var head = task.iframe.contentDocument.head;
+        head.appendChild(script);
+      }
+    } else {
+      task.iframe.src = task.data.url;
+    }
+	
+	container.style.textIndent = "0px";  
+  });
+
+})();
