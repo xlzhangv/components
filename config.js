4f4b91db51929c7c7858ff8b64d28fa4364ce998
Wenjin Liu <Wenjin Liu>
2017-05-30 17:28:20 +0800

    添加DataTable组件示例

--- config.js
+++ config.js
@@ -21,7 +21,87 @@ var navPanelTask = {
   taskType : "single"
 }
 
+var dataTableTask = {
+  data : {
+    header : [{
+        "name" : "列名1",
+        "key" : "key1"
+      }, {
+        "name" : "列名2",
+        "key" : "key2"
+      }, {
+        "name" : "列名3",
+        "key" : "key3"
+      }, {
+        "name" : "列名4",
+        "key" : "key4"
+      }
+    ],
+    records : [{
+        key1 : "NO.2232",
+        key2 : "abc1",
+        key3 : "xyz1",
+        key4 : "hello1"
+      }, {
+        key1 : "NO.2232",
+        key2 : "abc1",
+        key3 : "xyz1",
+        key4 : "hello1"
+      }, {
+        key1 : "NO.2232",
+        key2 : "abc1",
+        key3 : "xyz1",
+        key4 : "hello1"
+      }, {
+        key1 : "NO.2232",
+        key2 : "abc1",
+        key3 : "xyz1",
+        key4 : "hello1"
+      }, {
+        key1 : "NO.2232",
+        key2 : "abc1",
+        key3 : "xyz1",
+        key4 : "hello1"
+      }, {
+        key1 : "NO.2232",
+        key2 : "abc1",
+        key3 : "xyz1",
+        key4 : "hello1"
+      }, {
+        key1 : "NO.2232",
+        key2 : "abc1",
+        key3 : "xyz1",
+        key4 : "hello1"
+      }, {
+        key1 : "NO.2232",
+        key2 : "abc1",
+        key3 : "xyz1",
+        key4 : "hello1"
+      }, {
+        key1 : "NO.2232",
+        key2 : "abc1",
+        key3 : "xyz1",
+        key4 : "hello1"
+      }, {
+        key1 : "NO.2232",
+        key2 : "abc1",
+        key3 : "xyz1",
+        key4 : "hello1"
+      }, {
+        key1 : "NO.2232",
+        key2 : "abc1",
+        key3 : "xyz1",
+        key4 : "hello1"
+      }
+    ]
+  }
+}
+
 listLayoutTask.data[0] = {
   component : "NavPanel",
   task : navPanelTask
+}
+listLayoutTask.data[2] = {
+  component : "DataTable",
+  task : dataTableTask
 }
