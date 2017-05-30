9a761b89ad38ebadb52936d4aa5460cd7cf885b4
Wenjin Liu <Wenjin Liu>
2017-05-30 14:39:16 +0800

    ...

--- ItemListLayout/main.js
+++ ItemListLayout/main.js
@@ -6,9 +6,9 @@
   webCpu.regComponent("ItemListLayout", config, function (container, data, task) {
     task.containers = [];
     if (task.minWidth) {
-	  var max = Math.floor($(container).width() / task.minWidth) || 1;
-	  task.column = Math.min(max, task.column); 	
-	}
+      var max = Math.floor($(container).width() / task.minWidth) || 1;
+      task.column = Math.min(max, task.column);
+    }
     var w = 100 / (task.column || 1);
     var h = w * (task.ratio || 1);
     for (var i = 0; i < data.length; i++) {
@@ -21,12 +21,13 @@
       area.setAttribute("class", "ItemListLayout_container");
       div.appendChild(area);
       task.containers.push(area);
-      if (task.content && task.content.type === "component")
+      if (task.content && task.content.type === "component") {
         var _task = {
           container : area,
           data : data[i]
         }
-      webCpu.render(task.content.value, _task);
+        webCpu.render(task.content.value, _task);
+      }
     }
   });
 
